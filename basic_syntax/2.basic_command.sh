33fdac9c2e58ae92e3f24a0009af35fa11111fe1
JUNG-EUN1997 <rro0628@gmail.com>
2024-05-29 15:02:23 +0900

    vi commit this is vi commit

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -5,6 +5,12 @@ git status
 git add .
 
 # git add 특정파일 : 특정 파일 add
+git add 특정파일 위치
+
+# local repository에 반영 및 커밋이력 생성
+git commit -m "커밋명" -m "커밋내용"
+# vi 모드에서 작성
+git commit #엔터
 
 
 # commit 이력 확인 (local )
