5d81d93e8e096f244fe80a250d78280ae813fc76
JUNG-EUN1997 <rro0628@gmail.com>
2024-05-30 09:21:50 +0900

    fix

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -34,14 +34,32 @@ git checkout master
 git pull origin 브랜치
 
 # git 작업 취소
-# working directory에서의 취소
+
+# working directory에서의 취소 >> UI 수정 권장
+# 수정사항만 취소 > 새 파일은 포함되지 않음!
 git checkout .
+# 추가 파일도 수정 취소
+git clean --fdx
+# 수정 및 파일 동시 취소
+git checkout . | git clean --fdx
+
 # staging area에서의 취소
 git reset
+
 # commit 이후의 취소
 git reset HEAD~1
 git reset HEAD^
-# push 이후의 취소
 
+# push 이후의 취소
+git revert 커밋ID
+
+# 버전별 비교 명령어 : diff
+# A를 기준으로 B가 어떤 변경이 있는지를 비교
+git diff A B
+git diff 커밋아이디1 커밋아이디2
+git diff 브랜치1 브랜치2
+git diff d518b3 06b76 # 앞에 일부만 따서 진행해도 ㅇㅋ!
+
+git fetch origin master
 
 
